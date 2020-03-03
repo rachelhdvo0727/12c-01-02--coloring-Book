5f344b0336801c916da08a1a2d5f677e8af08b30
rachelvo <rach0727@stud.kea.dk>
2020-03-03 15:09:12 +0100

    added svg and colors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init(){
+    loadSVG();
+}
+
+async function loadSVG(){
+    const response = await fetch("donut-lines-fills.svg");
+    const mySVG = await response.text();
+    document.querySelector("#lineart").innerHTML= mySVG;
+}
