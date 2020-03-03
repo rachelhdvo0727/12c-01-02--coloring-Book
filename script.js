d4ad1120c55333442b41a05a92229ef21ff25089
rachelvo <rach0727@stud.kea.dk>
2020-03-03 17:25:43 +0100

    glasur and the Bun clickable

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", init);
+const HTML ={};
+let newfill;
 
 function init(){
+    HTML.colorBoxes = document.querySelectorAll(".color1, .color2, .color3, .color4, .color5, .color6, .color7, .color8");
+
     loadSVG();
 }
 
 async function loadSVG(){
-    const response = await fetch("donut-lines-fills.svg");
+    console.log("Donut loaded");
+    const response = await fetch("donut-transparent.svg");
     const mySVG = await response.text();
     document.querySelector("#lineart").innerHTML= mySVG;
+
+    //choose a color
+    HTML.colorBoxes.forEach((elm) => {
+        elm.addEventListener("click", clickABox);
+    });
+
+    document.querySelector("#glasur").addEventListener("click", clickGlasur);
+    document.querySelectorAll("#donut_fill .cls-2").forEach((element) => {
+        element.addEventListener("click", clickTheBun);
+    });
+}
+
+function clickABox(evt){
+    console.log("clickABox");
+    newfill = evt.target.getAttribute("fill");
+}
+
+function clickGlasur(evt){
+    console.log("clickGlasur");
+    evt.target.style.fill= newfill;
+}
+
+function clickTheBun(){
+    console.log("clickTheBun");
+    document.querySelectorAll("#donut_fill .cls-2").forEach((element) => {
+        element.style.fill = newfill;
+    });
 }
