75cc6e4da79e68123aaccb67bb623dcc11c2ba27
rachelvo <rach0727@stud.kea.dk>
2020-03-04 12:17:36 +0100

    added colorWheel + show current color

--- script.js
+++ script.js
@@ -3,15 +3,13 @@
 window.addEventListener("DOMContentLoaded", init);
 const HTML ={};
 let newfill;
+let colorfromWheel;
 
 function init(){
     HTML.colorBoxes = document.querySelectorAll(".color1, .color2, .color3, .color4, .color5, .color6, .color7, .color8");
+    HTML.chosenColor = document.querySelector("#colorwheel");
 
     loadSVG();
-    //choose a color
-    HTML.colorBoxes.forEach((element) => {
-        element.addEventListener("click", clickABox);
-    });
 }
 
 async function loadSVG(){
@@ -19,24 +17,59 @@ async function loadSVG(){
     const response = await fetch("donut-transparent.svg");
     const mySVG = await response.text();
     document.querySelector("#lineart").innerHTML= mySVG;
-
-    //click a path on svg
-    document.querySelectorAll("#donut_fill .cls-2").forEach((elm) => {
-        elm.addEventListener("click", clickTheDough);
-    });
-    document.querySelector("#glasur").addEventListener("click", clickGlasur);
-    document.querySelectorAll("#sprinkles_fill .cls-3").forEach((elm) => 
-    {
-        elm.addEventListener("click", function(){
-            console.log("clickKrymmel");
-            elm.style.fill = newfill;
+    
+    //choose a color
+    HTML.colorBoxes.forEach((element) => {
+        element.addEventListener("click", function(evt){
+            newfill = evt.target.getAttribute("fill");
+            getColorsFromBoxes();
+            });
         });
+        
+    //choose a color from the color wheel
+    HTML.chosenColor.addEventListener("change", function(evt){
+        colorfromWheel = evt.target.value;
+        console.log("colorfromWheel");
+        getColorFromTheWheel();
     });
 }
 
-function clickABox(evt){
-    console.log("clickABox");
-    newfill = evt.target.getAttribute("fill");
+function getColorsFromBoxes(){
+    console.log("getColorsFromBoxes");
+    //show the current color
+    document.querySelector(".currentcolor .colorbox").style.backgroundColor = newfill;
+
+    //click a path on svg
+    document.querySelectorAll("#donut_fill .cls-2").forEach((elm) => {
+        //all fills belong to the dough
+        elm.addEventListener("click", clickTheDough);
+});
+    document.querySelector("#glasur").addEventListener("click", clickGlasur);
+    document.querySelectorAll("#sprinkles_fill .cls-3").forEach((elm) => {
+        elm.addEventListener("click", function(){
+        console.log("clickKrymmel");
+        elm.style.fill = newfill;
+    })
+});
+}
+
+function getColorFromTheWheel(){
+    console.log("getColorFromTheWheel");
+    //show the current color
+    document.querySelector(".currentcolor .colorbox").style.backgroundColor = colorfromWheel;
+
+    //click a path on svg
+    document.querySelectorAll("#donut_fill .cls-2").forEach((elm) => {
+        //all fills belong to the dough
+        elm.addEventListener("click", clickTheDough2);
+    });
+    document.querySelector("#glasur").addEventListener("click", clickGlasur2);
+    document.querySelectorAll("#sprinkles_fill .cls-3").forEach((elm) => {
+        elm.addEventListener("click", function(){
+        console.log("clickKrymmel");
+        elm.style.fill = colorfromWheel;
+    })
+});
 }
 
 function clickTheDough(){
@@ -51,3 +84,14 @@ function clickGlasur(evt){
     evt.target.style.fill= newfill;
 }
 
+function clickTheDough2(){
+    console.log("clickTheDough2");
+    document.querySelectorAll("#donut_fill .cls-2").forEach((elm) => {
+        elm.style.fill = colorfromWheel;
+    });
+}
+
+function clickGlasur2(evt){
+    console.log("clickGlasur2");
+    evt.target.style.fill= colorfromWheel;
+}
